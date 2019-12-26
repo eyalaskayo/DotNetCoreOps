085bef4cc397d44b91514a4a338c66bc9fcd872e
eyalaskayo <eyal_askayo@yahoo.com>
2019-12-26 15:31:44 +0200

    rrr

--- CoreO/CoreO/Program.cs
+++ CoreO/CoreO/Program.cs
@@ -12,6 +12,10 @@ namespace CoreO
    
     public class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
